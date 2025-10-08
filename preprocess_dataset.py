9ec638eeb5739f7bf1eb1b9a9098a88a07ed0ced
minglii <minglii@umd.edu>
2025-10-08 10:56:28 -0400

    add pub split

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -17,6 +17,10 @@ def preprocess_dataset(input_file, output_file):
     for item in data:
         for question_key in item['questions']:
             question = item['questions'][question_key]
+
+            if question['text'] == '':
+                continue
+            
             text = f"{question['text']}\nOptions:\n(A) {question['options']['a']['text']}\n(B) {question['options']['b']['text']}\n(C) {question['options']['c']['text']}\n(D) {question['options']['d']['text']}\nCorrect Answer: {dict_lowercase[question['answer']]}\nReference Passage: {item['text']}"
 
             data_new.append({
@@ -33,5 +37,5 @@ def preprocess_dataset(input_file, output_file):
         json.dump(data_new, f, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
-    preprocess_dataset('Cambridge_mcq_test.json', 'Cambridge_mcq_test_Processed.json')
-    preprocess_dataset('Cambridge_mcq_train.json', 'Cambridge_mcq_train_Processed.json')
+    preprocess_dataset('Cambridge_mcq_test_pub.json', 'Cambridge_mcq_test_pub_Processed.json')
+    preprocess_dataset('Cambridge_mcq_train_pub.json', 'Cambridge_mcq_train_pub_Processed.json')
